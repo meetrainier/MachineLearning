73b08d5071076a920b55147726cd6cf5cd81c35e
meetrainier <manojcad@yahoo.com>
2020-02-02 14:53:48 -0800

    zip of hard coded Al having population more than 10k is found

--- zil_analyzer/zil_forecast_zip_state.py
+++ zil_analyzer/zil_forecast_zip_state.py
@@ -11,3 +11,27 @@ print(df_zip.head())
 
 df_zip_state = df_zip[df_zip["StateName"].astype(str).str.contains("AL")]
 print(df_zip_state.head())
+
+df_zip_state.sort_values(by=['ForecastYoYPctChange'], inplace=True, ascending=False)
+print(df_zip_state.head())
+
+min_population=10000
+
+#pop_file_path="data\UszipsShort.csv"
+pop_file_path = "data\ZipsShort.csv"
+zip_population_df = pd.read_csv(pop_file_path,sep=';')
+print(zip_population_df.head())
+print("type=",type(zip_population_df.loc[zip_population_df.zip == 1001,'population']))
+print("Population=")
+print(zip_population_df.loc[zip_population_df.zip == 1001,'population'].iloc[0])
+# def get_population(zip,df):
+	# for x in df['zip']:
+		# if x == zip:
+			# return 
+for x in df_zip_state['RegionName']:
+	print("Type of x =",type(x))
+	print("Type of zip_population_df.zip =",type(zip_population_df.zip))
+	if zip_population_df.loc[zip_population_df.zip == int(x),'population'].size == 1:
+		p = zip_population_df.loc[zip_population_df.zip == int(x),'population'].iloc[0]
+		if p < min_population: 
+			print('Qualified zip=',x)
