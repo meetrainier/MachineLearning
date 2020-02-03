cc1c322217833c592740ea06f1261a24931172dd
meetrainier <manojcad@yahoo.com>
2020-02-02 18:11:06 -0800

    can read med price data

--- zil_analyzer/zil_forecast_zip_state.py
+++ zil_analyzer/zil_forecast_zip_state.py
@@ -8,35 +8,48 @@ print(df.columns)
 df_zip = df[df["Region"].astype(str).str.contains("Zip")]
 print(df_zip.head())
 
+min_population=10000
+
+#pop_file_path="data\UszipsShort.csv"
+pop_file_path = "data\ZipsShort.csv"
+zip_population_df = pd.read_csv(pop_file_path,sep=';')
+
+med_price_file_path = "data\Sale_Prices_Zip.csv"
+zip_price_df = pd.read_csv(med_price_file_path,usecols=['RegionName','2019-11'])
+#zip_price_df = pd.read_csv(med_price_file_path,sep=';')
+print("zip_price_df=",zip_price_df.head())
+
 states = ['AL','AZ','CA','NV','OR','TX','WA']
+
 for s in states:
 	df_zip_state = df_zip[df_zip["StateName"].astype(str).str.contains(s)]
 	#print(df_zip_state.head())
 
 	df_zip_state.sort_values(by=['ForecastYoYPctChange'], inplace=True, ascending=False)
-	print(df_zip_state.head())
+	#print(df_zip_state.head())
 
-	min_population=10000
-
-	#pop_file_path="data\UszipsShort.csv"
-	pop_file_path = "data\ZipsShort.csv"
-	zip_population_df = pd.read_csv(pop_file_path,sep=';')
 	'''
 	print(zip_population_df.head())
 	print("type=",type(zip_population_df.loc[zip_population_df.zip == 1001,'population']))
 	print("Population=")
 	print(zip_population_df.loc[zip_population_df.zip == 1001,'population'].iloc[0])
 	'''
-	# def get_population(zip,df):
-		# for x in df['zip']:
-			# if x == zip:
-				# return 
 	count = 0
+	zips=[]
 	for x in df_zip_state['RegionName']:
 		#print("Type of x =",type(x))
 		#print("Type of zip_population_df.zip =",type(zip_population_df.zip))
 		if zip_population_df.loc[zip_population_df.zip == int(x),'population'].size == 1:
 			p = zip_population_df.loc[zip_population_df.zip == int(x),'population'].iloc[0]
 			if p < min_population and count < 10: 
-				print('Qualified zip=',x)
+				#print('Qualified zip=',x)
 				count=count+1
+				zips.append(x)
+	selected_rows=df_zip_state[df_zip_state.RegionName.isin(zips)]
+	print("The selected rows are:",selected_rows.head())
+#Next Plan: 	
+##1. Write function to return zips
+##2. Write function to modify zips based on min price
+##3. Write function to modify zips based on max price
+##5. Create df based on zip , include population , median price
+##6. Save the data-frame a csv
